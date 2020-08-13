5795c78d8cb518b6ad9f0cd8046aaf593da241e6
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2020-08-13 18:23:08 -0300

    reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import "../styles/styles.scss";
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
 
 let mobileMenu = new MobileMenu();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 if (module.hot) {
   module.hot.accept();
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,58 @@
+import throttle from "lodash/throttle";
+import debounce from "lodash/debounce";
+
+class RevealOnScroll {
+  constructor(els, limit) {
+    this.limit = limit;
+    this.itemsToReveal = els;
+    this.browserHeight = window.innerHeight;
+    this.hideInitially();
+
+    this.scrollThrottle = throttle(this.calcCaller, 500).bind(this);
+    this.events();
+  }
+
+  events() {
+    window.addEventListener("scroll", this.scrollThrottle);
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        this.browserHeight = window.innerHeight;
+      }, 333)
+    );
+  }
+
+  calcCaller() {
+    this.itemsToReveal.forEach((el) => {
+      if (el.isRevealed == false) {
+        this.calculateIfScrolledTo(el);
+      }
+    });
+  }
+
+  calculateIfScrolledTo(el) {
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      let scrollPercent =
+        (el.getBoundingClientRect().y / this.browserHeight) * 100;
+      if (scrollPercent < this.limit) {
+        el.classList.add("reveal-item--is-visible");
+        el.isRevealed = true;
+
+        if (el.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle);
+        }
+      }
+    }
+  }
+
+  hideInitially() {
+    this.itemsToReveal.forEach((el) => {
+      el.classList.add("reveal-item");
+
+      el.isRevealed = false;
+    });
+
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
+  }
+}
+export default RevealOnScroll;
