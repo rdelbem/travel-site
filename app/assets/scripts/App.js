cc38ef323e5140a697e1afe42a59e669d04ec543
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2020-08-13 23:56:28 -0300

    Completado sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import "../styles/styles.scss";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
 
+let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,13 +2,12 @@ import throttle from "lodash/throttle";
 import debounce from "lodash/debounce";
 
 class RevealOnScroll {
-  constructor(els, limit) {
-    this.limit = limit;
+  constructor(els, thresholdPercent) {
+    this.thresholdPercent = thresholdPercent;
     this.itemsToReveal = els;
     this.browserHeight = window.innerHeight;
     this.hideInitially();
-
-    this.scrollThrottle = throttle(this.calcCaller, 500).bind(this);
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
@@ -17,12 +16,14 @@ class RevealOnScroll {
     window.addEventListener(
       "resize",
       debounce(() => {
+        console.log("Resize just ran");
         this.browserHeight = window.innerHeight;
       }, 333)
     );
   }
 
   calcCaller() {
+    console.log("Scroll function ran");
     this.itemsToReveal.forEach((el) => {
       if (el.isRevealed == false) {
         this.calculateIfScrolledTo(el);
@@ -32,12 +33,12 @@ class RevealOnScroll {
 
   calculateIfScrolledTo(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop) {
+      console.log("Element was calculated");
       let scrollPercent =
-        (el.getBoundingClientRect().y / this.browserHeight) * 100;
-      if (scrollPercent < this.limit) {
+        (el.getBoundingClientRect().top / this.browserHeight) * 100;
+      if (scrollPercent < this.thresholdPercent) {
         el.classList.add("reveal-item--is-visible");
         el.isRevealed = true;
-
         if (el.isLastItem) {
           window.removeEventListener("scroll", this.scrollThrottle);
         }
@@ -48,11 +49,10 @@ class RevealOnScroll {
   hideInitially() {
     this.itemsToReveal.forEach((el) => {
       el.classList.add("reveal-item");
-
       el.isRevealed = false;
     });
-
     this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
   }
 }
+
 export default RevealOnScroll;
