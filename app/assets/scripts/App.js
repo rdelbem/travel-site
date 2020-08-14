be0fe6278494eb4d4231ed993da00192b2efcde3
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2020-08-14 01:13:00 -0300

    modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,29 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-let stickyHeader = new StickyHeader();
-let mobileMenu = new MobileMenu();
+new StickyHeader();
+new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+
+let modal;
+
+document.querySelectorAll(".open-modal").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (typeof modal == "undefined") {
+      import(/*webpackChuckName: "modal"*/ "./modules/Modal")
+        .then((x) => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("err"));
+    } else {
+      modal.openTheModal();
+    }
+  });
+});
 
 if (module.hot) {
   module.hot.accept();
